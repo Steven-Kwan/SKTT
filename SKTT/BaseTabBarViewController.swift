c5701acc77e00e84cd8eb43763000c783672fe82
Kwan <kwan@KwandeMacBook-Pro-2.local>
2019-05-30 14:06:27 +0800

    first update

---
+++ SKTT/BaseTabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseTabBarViewController.swift
+//  SKTT
+//
+//  Created by Kwan on 2019/5/30.
+//  Copyright © 2019 Kwan. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SKTT/constant.swift
@@ -0,0 +1,9 @@
+//
+//  constant.swift
+//  SKTT
+//
+//  Created by Kwan on 2019/5/30.
+//  Copyright © 2019 Kwan. All rights reserved.
+//
+
+import Foundation
